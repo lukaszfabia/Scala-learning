a2e6eb284f9b9ec6ecf60c143b72e7949763bbf7
Lukasz <ufabia03@gmail.com>
2023-08-31 20:12:26 +0200

    new functions/methods

--- src/scala/learning/tasks/lists/Lists.scala
+++ src/scala/learning/tasks/lists/Lists.scala
@@ -1,4 +1,6 @@
-package scala.learning.tasks
+package scala.learning.tasks.lists
+
+import scala.annotation.tailrec
 
 object Lists {
   def main(args: Array[String]): Unit = {
@@ -8,6 +10,11 @@ object Lists {
     println(sqrtList(List(1, 2, 3, 4, 5)))
     println(palindrome(List(1, 2, 3, 2, 1)))
     println(listLength(List(1, 2, 12, 1, 2, 32, 1, 3, 13, 12, 312)))
+    println(reverseList(List(1,2,3)))
+    println(removeDuplicates(List(1,2,32,3,1,123,3,1)))
+    println(findMaxValue(List(0,1,2,32,3,14,1)))
+    println(filterOddNumbers(List(1,2,3,4,5,6,7,8)))
+    println(fibonacciSequence(8))
   }
 
   /*
@@ -81,4 +88,65 @@ object Lists {
     }
   }
 
+  private def reverseList[T](list: List[T]): List[T] = {
+    if (list != Nil) {
+      reverseList(list.tail) ::: List(list.head)
+    } else {
+      Nil
+    }
+  }
+
+  private def filterOddNumbers(list: List[Int]): List[Int] = {
+    if (list.nonEmpty) {
+      if (list.head % 2 == 0) {
+        List(list.head) ::: filterOddNumbers(list.tail)
+      } else {
+        filterOddNumbers(list.tail)
+      }
+    } else {
+      Nil
+    }
+  }
+
+  private var result = 0
+  @tailrec
+  private def findMaxValue(list: List[Int]): Int = {
+    if (list.nonEmpty) {
+      if (result < list.head) {
+        result = list.head
+        findMaxValue(list.tail)
+      } else {
+        findMaxValue(list.tail)
+      }
+    } else {
+      result
+    }
+  }
+
+  private def removeDuplicates[T](list: List[T]): List[T] = {
+    if (list.nonEmpty) {
+      if (list.tail.contains(list.head)) {
+        removeDuplicates(list.tail)
+      } else {
+        List(list.head) ::: removeDuplicates(list.tail)
+      }
+    } else {
+      Nil
+    }
+  }
+
+
+  private def fibonacciSequence(n : Int): List[Int] = {
+    if (n>0){
+      if (n==1){
+        List(0)
+      }else if (n==2){
+        List(0,1)
+      }else{
+        fibonacciSequence(n-1) ::: List(fibonacciSequence(n-1).last + fibonacciSequence(n-2).last)
+      }
+    }else{
+      Nil
+    }
+  }
 }
