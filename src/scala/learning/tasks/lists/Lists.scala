2ee975d021226abc1671a748eeebd4b5861231d5
Lukasz <ufabia03@gmail.com>
2023-09-01 09:44:45 +0200

    fixed some functions and updated README.md

--- src/scala/learning/tasks/lists/Lists.scala
+++ src/scala/learning/tasks/lists/Lists.scala
@@ -8,13 +8,13 @@ object Lists {
     println(mergeList(List(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9))))
     println(replicate("Yes", 3))
     println(sqrtList(List(1, 2, 3, 4, 5)))
-    println(palindrome(List(1, 2, 3, 2, 1)))
+    println("Palindrome:  " + palindrome(List(true, false, false, true)))
     println(listLength(List(1, 2, 12, 1, 2, 32, 1, 3, 13, 12, 312)))
-    println(reverseList(List(1,2,3)))
-    println(removeDuplicates(List(1,2,32,3,1,123,3,1)))
-    println(findMaxValue(List(0,1,2,32,3,14,1)))
-    println(filterOddNumbers(List(1,2,3,4,5,6,7,8)))
-    println(fibonacciSequence(8))
+    println(reverseList(List(1, 2, 3)))
+    println("Remove duplicates: " + removeDuplicates(List(1, 2, 32, 3, 1, 123, 3, 1)))
+    println(findMaxValue(List(0, 1, 2, 32, 3, 14, 1)))
+    println(filterOddNumbers(List(1, 2, 3, 4, 5, 6, 7, 8)))
+    println(fibonacciSequence(4))
   }
 
   /*
@@ -65,19 +65,23 @@ object Lists {
   /*
   @param list - lista liczb
    */
-
-  //  :: służy do dodawania elementu na początek listy.
-  //  ::: służy do łączenia dwóch list w jedną.
   private def sqrtList(list: List[Int]): List[Int] = {
     if (list.nonEmpty) {
-      list.head * list.head :: sqrtList(list.tail)
+      List(list.head * list.head) ::: sqrtList(list.tail)
     } else {
       Nil
     }
   }
 
+  @tailrec
   private def palindrome[T](list: List[T]): Boolean = {
-    list == list.reverse
+    if (list.length <= 1) {
+      true
+    } else if (list.head == list.last) {
+      palindrome(list.tail.init)
+    } else {
+      false
+    }
   }
 
   private def listLength[T](list: List[T]): Int = {
@@ -108,16 +112,23 @@ object Lists {
     }
   }
 
-  private var result = 0
-  @tailrec
   private def findMaxValue(list: List[Int]): Int = {
+    // instead of declaring var result = 0 out of function, in object scope
+    var result = Integer.MIN_VALUE
     if (list.nonEmpty) {
-      if (result < list.head) {
-        result = list.head
-        findMaxValue(list.tail)
-      } else {
-        findMaxValue(list.tail)
+      @tailrec
+      def maxValue(list: List[Int]): Int = {
+        if (list.nonEmpty) {
+          if (list.head > result) {
+            result = list.head
+          }
+          maxValue(list.tail)
+        } else {
+          result
+        }
       }
+
+      maxValue(list)
     } else {
       result
     }
@@ -136,16 +147,16 @@ object Lists {
   }
 
 
-  private def fibonacciSequence(n : Int): List[Int] = {
-    if (n>0){
-      if (n==1){
+  private def fibonacciSequence(n: Int): List[Int] = {
+    if (n > 0) {
+      if (n == 1) {
         List(0)
-      }else if (n==2){
-        List(0,1)
-      }else{
-        fibonacciSequence(n-1) ::: List(fibonacciSequence(n-1).last + fibonacciSequence(n-2).last)
+      } else if (n == 2) {
+        List(0, 1)
+      } else {
+        fibonacciSequence(n - 1) ::: List(fibonacciSequence(n - 1).last + fibonacciSequence(n - 2).last)
       }
-    }else{
+    } else {
       Nil
     }
   }
